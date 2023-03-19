5cfc15e1eebca17d9ad8a2b8bb47bff8609b3831
221RDB243 <125822361+Bonqs@users.noreply.github.com>
2023-03-20 00:26:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-def parallel_processing(num_threads, num_jobs, job_times):
+def simulate_parallel_processing(num_threads, num_jobs, job_times):
     output = []
     threads = [0] * num_threads
+    current_job = 0
+    current_time = 0
+    while current_job < num_jobs:
+        for i in range(num_threads):
+            if threads[i] == 0:
+                threads[i] = job_times[current_job]
+                output.append((i, current_time))
+                current_job += 1
+                if current_job == num_jobs:
+                    break
 
-    for i, job_time in enumerate(job_times):
-        thread_index = threads.index(min(threads))
-        output.append((thread_index, threads[thread_index]))
-        threads[thread_index] += job_time
+        for i in range(num_threads):
+            if threads[i] > 0:
+                threads[i] -= 1
+
+        current_time += 1
 
     return output
 
+
 def main():
-    num_threads = int(input())
-    num_jobs = int(input())
+    num_threads, num_jobs = map(int, input().split())
+
     job_times = list(map(int, input().split()))
 
-    results = parallel_processing(num_threads, num_jobs, job_times)
+    result = simulate_parallel_processing(num_threads, num_jobs, job_times)
 
-    for thread_index, start_time in results:
-        print(f"Job assigned to thread {thread_index} at time {start_time} seconds")
+    for thread_num, start_time in result:
+        print(thread_num, start_time)
+
 
 if __name__ == "__main__":
     main()
